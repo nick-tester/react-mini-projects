69b97f02fe73defc44dd12aab4099d3dfba655e8
Romenick Tester <romenick.tester@gmail.com>
2021-12-22 01:59:57 +0000

    project 2: tour component

--- src/pages/Tours/Tour.js
+++
@@ -1,7 +0,0 @@
-
-
-const Tour = () => {
-    return <h4>tour component</h4>
-};
-
-export default Tour;
--- src/pages/Tours/index.js
+++ src/pages/Tours/index.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 
-import Tour from "./Tour";
 import CSS from "./index.module.css";
 
 const url = "https://course-api.com/react-tours-project";
 
+// TOURS PAGE
 const Tours = () => {
     const [loading, setLoading] = useState(false);
     const [tours, setTours] = useState([]);
@@ -30,20 +30,43 @@ const Tours = () => {
     return (
         <main className={CSS.main}>
             {loading ? <Loading /> : (
-                <>
-                    {tours.map(tour => <Tour key={tour.id} />)}
-                </>
+                <section>
+                    <div className={CSS.title}>
+                        <h2>our tours</h2>
+                        <div className={CSS.underline}></div>
+                    </div>
+                    <div>
+                        {tours.map(tour => <Tour key={tour.id} {...tour} />)}
+                    </div>
+                </section>
             )}
         </main>
     )
 };
 
-
+// LOADING COMPONENT
 const Loading = () => {
     return (
         <div className={CSS.loading}>
             <h1>loading...</h1>
         </div>
+    )
+};
+
+// TOUR COMPONENT
+const Tour = ({ id, image, info, price, name }) => {
+    return (
+        <article className={CSS.single__tour}>
+            <img src={image} alt={name} />
+            <footer>
+                <div className={CSS.tour__info}>
+                    <h4>{name}</h4>
+                    <h4 className={CSS.tour__price}>£{price}</h4>
+                </div>
+                <p>{info}</p>
+                <button className={CSS.delete__btn}>not interested</button>
+            </footer>
+        </article>
     )
 };
 
