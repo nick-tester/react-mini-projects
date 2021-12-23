d33384444d12c26a4e8b3fe53e1c25a89ae69706
Romenick Tester <romenick.tester@gmail.com>
2021-12-23 19:26:00 +0000

    project 3: review component

--- src/pages/03/Reviews.js
+++ src/pages/03/Reviews.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+import people from "./data";
 import CSS from "./Reviews.module.css";
 
 const Reviews = () => {
@@ -7,14 +10,36 @@ const Reviews = () => {
                 <h2>our reviews</h2>
                 <div className={CSS.underline}></div>
             </div>
-            <Review />
+            <div className={CSS.container}>
+                <Review />
+            </div>
         </main>
     );
 };
 
 
 const Review = () => {
-    return <h3>review component</h3>
+    const [index, setIndex] = useState(0);
+    const { name, job, image, text, info } = people[index];
+
+    return (
+        <article className={CSS.review}>
+            <div className={CSS.img_container}>
+                <img src={image} alt={name} className={CSS.person_img} />
+                <span className={CSS.quote_icon}>
+                    <FaQuoteRight />
+                </span>
+            </div>
+            <h4 className={CSS.author}>{name}</h4>
+            <p className={CSS.job}>{job}</p>
+            <p className={info}>{text}</p>
+            <div className={CSS.button_container}>
+                <button className={CSS.prev_btn}><FaChevronLeft /></button>
+                <button className={CSS.next_btn}><FaChevronRight /></button>
+            </div>
+            <button className={CSS.random_btn}>suprise me</button>
+        </article>
+    )
 };
 
 
