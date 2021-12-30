ee273314f4c88eb5672303496c793a9bf145b2f6
Romenick Tester <romenick.tester@gmail.com>
2021-12-30 23:47:55 +0000

    project 4: accordion

--- src/pages/04/Accordion.js
+++ src/pages/04/Accordion.js
@@ -1,12 +1,42 @@
+import { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+import CSS from "./Accordion.module.css";
+import data from "./data";
 
 const Accordion = () => {
+    // eslint-disable-next-line
+    const [questions, setQuestions] = useState([...data]);
+
     return (
-        <>
-            <h4>Accordion component</h4>
-        </>
+        <main className={CSS.main}>
+            <div className={CSS.container}>
+                <h3>question and answers about login</h3>
+                <section className={CSS.info}>
+                    {questions.map(question => {
+                        return <SingleQestion key={question.id} {...question} />
+                    })}
+                </section>
+            </div>
+        </main>
     )
 };
 
 
+const SingleQestion = ({ title, info }) => {
+    const [showInfo, setShowInfo] = useState(false);
+
+    return (
+        <article className={CSS.question}>
+            <header>
+                <h4>{title}</h4>
+                <button className={CSS.btn} onClick={() => setShowInfo(!showInfo)}>
+                    {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+                </button>
+            </header>
+            {showInfo && <p>{info}</p>}
+        </article>
+    )
+}
 
 export default Accordion;
