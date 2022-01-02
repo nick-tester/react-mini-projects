22e6fa803207c74b92e342ef77802efac688e345
Romenick Tester <romenick.tester@gmail.com>
2022-01-02 16:43:30 +0000

    project 06: fetch data

--- src/pages/06/Tabs.js
+++ src/pages/06/Tabs.js
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
 import CSS from "./Tabs.module.css";
 
 const url = "https://course-api.com/react-tabs-project";
 
 const Tabs = () => {
+    const [loading, setLoading] = useState(false);
+    const [jobs, setJobs] = useState([]);
+
+    const fetchData = async () => {
+        setLoading(true);
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+
+            console.log(data);
+            setJobs(data);
+        } catch (err) {
+            console.error(err.message);
+        }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    if (loading) {
+        return <h2>loading...</h2>
+    }
+
     return (
         <h2>
             tabs app
